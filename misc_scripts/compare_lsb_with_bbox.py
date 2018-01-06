a18bbb3844c78a27e911e569c9cbd904af0a5d7a
derwind <uncertainty_principle_ss@yahoo.co.jp>
2018-01-06 16:05:24 +0900

    show glyph which causes maxdiff

--- misc_scripts/compare_lsb_with_bbox.py
+++ misc_scripts/compare_lsb_with_bbox.py
@@ -5,12 +5,26 @@ import os, sys, re
 from fontTools.ttLib import TTFont
 from fontTools.pens.boundsPen import BoundsPen, ControlBoundsPen
 
+class ConcordanceInfo(object):
+    def __init__(self):
+        self.glyphs = 0
+        self.concordant_glyphs = 0
+        self.maxdiff = 0
+        self.maxdiff_gname = None
+
+    def update(self, diff, gname):
+        self.glyphs += 1
+        if diff <= 1:
+            self.concordant_glyphs += 1
+        elif diff > self.maxdiff:
+            self.maxdiff = round(diff, 2)
+            self.maxdiff_gname = gname
+
 def calc_concordance_and_maxdiff(font, penclass):
     hmtx = font["hmtx"]
     gs = font.getGlyphSet()
 
-    cnt = concordance_cnt = 0
-    maxdiff = 0
+    info = ConcordanceInfo()
     for gname in font.getGlyphOrder():
         g = gs[gname]
 
@@ -22,13 +36,9 @@ def calc_concordance_and_maxdiff(font, penclass):
         left, _, _, _ = pen.bounds
         lsb = hmtx.metrics[gname][1]
 
-        cnt += 1
         diff = abs(left - lsb)
-        if diff <= 1:
-            concordance_cnt += 1
-        elif diff > maxdiff:
-            maxdiff = diff
-    return cnt, concordance_cnt, round(maxdiff, 2)
+        info.update(diff, gname)
+    return info
 
 def lsb_means_left_of_control_bbox_or_strict_bbox():
     font_path = sys.argv[1]
@@ -36,13 +46,13 @@ def lsb_means_left_of_control_bbox_or_strict_bbox():
     font = TTFont(font_path, fontNumber=0)
     flag_1 = (font["head"].flags >> 1 & 0x1)
 
-    cnt, cb_cnt, cb_maxdiff = calc_concordance_and_maxdiff(font, ControlBoundsPen)
-    cnt, b_cnt, b_maxdiff = calc_concordance_and_maxdiff(font, BoundsPen)
+    cb_info = calc_concordance_and_maxdiff(font, ControlBoundsPen)
+    b_info = calc_concordance_and_maxdiff(font, BoundsPen)
 
     print "[{}]".format(basename)
     print "  head.flags[1]: {}".format(flag_1)
-    print "  ControlBounds: {}/{} ({}%); maxdiff={}".format(cb_cnt, cnt, round(100.*cb_cnt/cnt, 2), cb_maxdiff)
-    print "  Bounds       : {}/{} ({}%); maxdiff={}".format(b_cnt, cnt, round(100.*b_cnt/cnt, 2), b_maxdiff)
+    print "  ControlBounds: {}/{} ({}%); maxdiff={} ({})".format(cb_info.concordant_glyphs, cb_info.glyphs, round(100.*cb_info.concordant_glyphs/cb_info.glyphs, 2), cb_info.maxdiff, cb_info.maxdiff_gname)
+    print "  Bounds       : {}/{} ({}%); maxdiff={} ({})".format(b_info.concordant_glyphs, b_info.glyphs, round(100.*b_info.concordant_glyphs/b_info.glyphs, 2), b_info.maxdiff, b_info.maxdiff_gname)
 
 def main():
     lsb_means_left_of_control_bbox_or_strict_bbox()
