41f70b963a029017983ac4c35dd09150bd22abf3
derwind <uncertainty_principle_ss@yahoo.co.jp>
2018-01-06 12:46:24 +0900

    show bit 1 of head.flags i.e. "x position of left most black bit is LSB."

--- misc_scripts/compare_lsb_with_bbox.py
+++ misc_scripts/compare_lsb_with_bbox.py
@@ -34,11 +34,13 @@ def lsb_means_left_of_control_bbox_or_strict_bbox():
     font_path = sys.argv[1]
     basename = os.path.basename(font_path)
     font = TTFont(font_path, fontNumber=0)
+    flag_1 = (font["head"].flags >> 1 & 0x1)
 
     cnt, cb_cnt, cb_maxdiff = calc_concordance_and_maxdiff(font, ControlBoundsPen)
     cnt, b_cnt, b_maxdiff = calc_concordance_and_maxdiff(font, BoundsPen)
 
     print "[{}]".format(basename)
+    print "  head.flags[1]: {}".format(flag_1)
     print "  ControlBounds: {}/{} ({}%); maxdiff={}".format(cb_cnt, cnt, round(100.*cb_cnt/cnt, 2), cb_maxdiff)
     print "  Bounds       : {}/{} ({}%); maxdiff={}".format(b_cnt, cnt, round(100.*b_cnt/cnt, 2), b_maxdiff)
 
