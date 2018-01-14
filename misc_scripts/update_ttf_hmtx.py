b24e41bd83829065ada30fa73fce23171b882d16
derwind <uncertainty_principle_ss@yahoo.co.jp>
2018-01-14 16:54:28 +0900

    add functionality of updating vmtx

--- misc_scripts/update_ttf_hmtx.py
+++ misc_scripts/update_ttf_hmtx.py
@@ -6,13 +6,14 @@ import argparse
 from fontTools.ttLib import TTFont
 
 class HtmxUpdater(object):
-    def __init__(self, in_font, out_font):
+    def __init__(self, in_font, out_font, update_vmtx=False):
         self.in_font = in_font
         self.out_font = out_font
+        self.update_vmtx = update_vmtx
 
     def run(self):
         font = TTFont(self.in_font)
-        #gs = font.getGlyphSet()
+        gs = font.getGlyphSet()
         glyf = font["glyf"]
         hmtx = font["hmtx"]
         for gname, (adw, lsb) in hmtx.metrics.items():
@@ -20,6 +21,19 @@ class HtmxUpdater(object):
             # obtain xMin
             g.recalcBounds(glyf)
             hmtx.metrics[gname] = (adw, g.xMin)
+        if self.update_vmtx:
+            from fontTools.pens.boundsPen import BoundsPen
+            vmtx = font["vmtx"]
+            for gname, (adh, tsb) in vmtx.metrics.items():
+                g = glyf[gname]
+                # obtain yMax
+                g.recalcBounds(glyf)
+                pen = BoundsPen(gs)
+                g.draw(pen, glyf)
+                if pen.bounds is None:
+                    continue
+                left, bottom, right, top = pen.bounds
+                vmtx.metrics[gname] = (adh, top + tsb - g.yMax)
 
         font.save(self.out_font)
 
@@ -31,6 +45,8 @@ def get_args():
                         help="input font")
     parser.add_argument("-o", "--output", dest="out_font", default=None,
                         help="output font")
+    parser.add_argument("-v", "--vmtx", dest="update_vmtx", action="store_true",
+                        help="also update vmtx")
 
     args = parser.parse_args()
 
@@ -46,7 +62,7 @@ def main():
         print>>sys.stderr, "only TTF is supported"
         sys.exit(1)
 
-    tool = HtmxUpdater(args.in_font, args.out_font)
+    tool = HtmxUpdater(args.in_font, args.out_font, args.update_vmtx)
     sys.exit(tool.run())
 
 if __name__ == "__main__":
