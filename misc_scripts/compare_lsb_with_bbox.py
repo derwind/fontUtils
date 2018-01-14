d1e7fede49e9df7406839f4f2f93b7fc8593c92f
derwind <uncertainty_principle_ss@yahoo.co.jp>
2018-01-14 21:27:12 +0900

    compare xMin/yMax with control bbox

--- misc_scripts/compare_lsb_with_bbox.py
+++ misc_scripts/compare_lsb_with_bbox.py
@@ -26,10 +26,14 @@ def calc_concordance_and_maxdiff(font, penclass):
 
     info = ConcordanceInfo()
     for gname in font.getGlyphOrder():
-        g = gs[gname]
-
         pen = penclass(gs)
-        g.draw(pen)
+        if "glyf" in font:
+            glyf = font["glyf"]
+            g = glyf[gname]
+            g.draw(pen, glyf)
+        else:
+            g = gs[gname]
+            g.draw(pen)
         if pen.bounds is None:
             continue
 
@@ -37,6 +41,25 @@ def calc_concordance_and_maxdiff(font, penclass):
         lsb = hmtx.metrics[gname][1]
 
         diff = abs(left - lsb)
+        info.update(diff, gname)
+    return info
+
+def calc_concordance_of_xMin_yMax_and_control_bbox(font):
+    gs = font.getGlyphSet()
+
+    info = ConcordanceInfo()
+    for gname in font.getGlyphOrder():
+        pen = ControlBoundsPen(gs)
+        glyf = font["glyf"]
+        g = glyf[gname]
+        g.draw(pen, glyf)
+        if pen.bounds is None:
+            continue
+
+        left, _, _, top = pen.bounds
+        g.recalcBounds(glyf)
+
+        diff = abs(left - g.xMin) + abs(top - g.yMax)
         info.update(diff, gname)
     return info
 
@@ -50,9 +73,16 @@ def lsb_means_left_of_control_bbox_or_strict_bbox():
     b_info = calc_concordance_and_maxdiff(font, BoundsPen)
 
     print "[{}]".format(basename)
+    print " LSB vs bbox.left"
     print "  head.flags[1]: {}".format(flag_1)
     print "  ControlBounds: {}/{} ({}%); maxdiff={} ({})".format(cb_info.concordant_glyphs, cb_info.glyphs, round(100.*cb_info.concordant_glyphs/cb_info.glyphs, 2), cb_info.maxdiff, cb_info.maxdiff_gname)
     print "  Bounds       : {}/{} ({}%); maxdiff={} ({})".format(b_info.concordant_glyphs, b_info.glyphs, round(100.*b_info.concordant_glyphs/b_info.glyphs, 2), b_info.maxdiff, b_info.maxdiff_gname)
+    if "glyf" not in font:
+        return
+
+    info = calc_concordance_of_xMin_yMax_and_control_bbox(font)
+    print " xMin/yMax vs control bbox"
+    print "  ControlBounds vs xMin/yMax: {}/{} ({}%); maxdiff={} ({})".format(info.concordant_glyphs, info.glyphs, round(100.*info.concordant_glyphs/info.glyphs, 2), info.maxdiff, info.maxdiff_gname)
 
 def main():
     lsb_means_left_of_control_bbox_or_strict_bbox()
