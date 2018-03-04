8e85c6def3e334cc38897b702892da69ae8bf800
derwind <diesonneundderwind@gmail.com>
2018-03-05 02:38:33 +0900

    try to reimplement by C#

---
+++ otfanalyzer_csharp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace otfanalyzer_csharp
+{
+    class Program
+    {
+        static void showUsage(string programName)
+        {
+            System.Console.WriteLine("[usage] {0} OTF_PATH\n", programName);
+        }
+
+        static void Main(string[] args)
+        {
+            if ( args.Length < 1 )
+            {
+                showUsage(Environment.GetCommandLineArgs()[0]);
+                return;
+            }
+        }
+    }
+}
---
+++ otfanalyzer_csharp/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("otfanalyzer_csharp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("otfanalyzer_csharp")]
+[assembly: AssemblyCopyright("Copyright c  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("d0884c2a-5f46-47a2-a566-45155186861f")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
